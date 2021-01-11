7136ad9aae6072482419812463bd4d323b3432a7
Deepa Subramanyam <68331226+deepasubramanyam@users.noreply.github.com>
2021-01-11 12:06:58 +0530

    concatenate two strings without strcat()

---
+++ concate_strings.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+void main()
+{
+  char str1[100],str2[100];
+  int i,j;
+  scanf("%s %s",&str1,&str2);
+  for(i=0;str1[i]!='\0';i++)
+  {
+
+  }
+  for(j=0;str2[j]!='\0';i++,j++)
+  {
+      str1[i]=str2[j];
+  }
+  str1[i]='\0';
+  printf("%s",str1);
+}
